8501175a94994124db55065ffc1faeef920e050b
muhammadfaizan7986 <muhammadfaizan7986@gmail.com>
2024-08-30 00:26:08 +0500

    latatest code in this branch

--- src/app/our-taxi/taxi-lists/page.js
+++ src/app/our-taxi/taxi-lists/page.js
@@ -1,5 +1,323 @@
 import React from "react";
 import Banner from "../../../components/common/banner/Banner";
+const pricingData = [
+  {
+    title: "CAMRY SONATA",
+    location: "Jeddah Airport to Makkah Hotel",
+    initialCharge: "250/SAR",
+  },
+  {
+    title: "H1 HYUNDAI",
+    location: "Jeddah Airport to Makkah Hotel",
+    initialCharge: "350/SAR",
+  },
+  {
+    title: "GMC 2016 to 2020 model",
+    location: "Jeddah Airport to Makkah Hotel",
+    initialCharge: "400/SAR",
+  },
+  {
+    title: "GMC 2022 model",
+    location: "Jeddah Airport to Makkah Hotel",
+    initialCharge: "550/SAR",
+  },
+  {
+    title: "HIACE",
+    location: "Jeddah Airport to Makkah Hotel",
+    initialCharge: "400/SAR",
+  },
+  {
+    title: "COASTER",
+    location: "Jeddah Airport to Makkah Hotel",
+    initialCharge: "800/SAR",
+  },
+  {
+    title: "BUS",
+    location: "Jeddah Airport to Makkah Hotel",
+    initialCharge: "800/SAR",
+  },
+  {
+    title: "CAMRY SONATA",
+    location: "Makkah Hotel to Jeddah Airport",
+    initialCharge: "200/SAR",
+  },
+  {
+    title: "H1 HYUNDAI",
+    location: "Makkah Hotel to Jeddah Airport",
+    initialCharge: "300/SAR",
+  },
+  {
+    title: "GMC 2016 to 2020 model",
+    location: "Makkah Hotel to Jeddah Airport",
+    initialCharge: "400/SAR",
+  },
+  {
+    title: "GMC 2022 model",
+    location: "Makkah Hotel to Jeddah Airport",
+    initialCharge: "500/SAR",
+  },
+  {
+    title: "HIACE",
+    location: "Makkah Hotel to Jeddah Airport",
+    initialCharge: "400/SAR",
+  },
+  {
+    title: "COASTER",
+    location: "Makkah Hotel to Jeddah Airport",
+    initialCharge: "700/SAR",
+  },
+  {
+    title: "BUS",
+    location: "Makkah Hotel to Jeddah Airport",
+    initialCharge: "800/SAR",
+  },
+  {
+    title: "CAMRY SONATA",
+    location: "Makkah Hotel to Madina Hotel",
+    initialCharge: "450/SAR",
+  },
+  {
+    title: "H1 HYUNDAI",
+    location: "Makkah Hotel to Madina Hotel",
+    initialCharge: "550/SAR",
+  },
+  {
+    title: "GMC 2016 to 2020 model",
+    location: "Makkah Hotel to Madina Hotel",
+    initialCharge: "800/SAR",
+  },
+  {
+    title: "GMC 2022 model",
+    location: "Makkah Hotel to Madina Hotel",
+    initialCharge: "1000/SAR",
+  },
+  {
+    title: "HIACE",
+    location: "Makkah Hotel to Madina Hotel",
+    initialCharge: "650/SAR",
+  },
+  {
+    title: "COASTER",
+    location: "Makkah Hotel to Madina Hotel",
+    initialCharge: "1100/SAR",
+  },
+  {
+    title: "BUS",
+    location: "Makkah Hotel to Madina Hotel",
+    initialCharge: "1400/SAR",
+  },
+  {
+    title: "CAMRY SONATA",
+    location: "Madina Hotel to Makkah Hotel",
+    initialCharge: "450/SAR",
+  },
+  {
+    title: "H1 HYUNDAI",
+    location: "Madina Hotel to Makkah Hotel",
+    initialCharge: "550/SAR",
+  },
+  {
+    title: "GMC 2016 to 2020 model",
+    location: "Madina Hotel to Makkah Hotel",
+    initialCharge: "800/SAR",
+  },
+  {
+    title: "GMC 2022 model",
+    location: "Madina Hotel to Makkah Hotel",
+    initialCharge: "1000/SAR",
+  },
+  {
+    title: "HIACE",
+    location: "Madina Hotel to Makkah Hotel",
+    initialCharge: "650/SAR",
+  },
+  {
+    title: "COASTER",
+    location: "Madina Hotel to Makkah Hotel",
+    initialCharge: "1100/SAR",
+  },
+  {
+    title: "BUS",
+    location: "Madina Hotel to Makkah Hotel",
+    initialCharge: "1400/SAR",
+  },
+  {
+    title: "CAMRY SONATA",
+    location: "Madina Airport to Madina Hotel",
+    initialCharge: "150/SAR",
+  },
+  {
+    title: "H1 HYUNDAI",
+    location: "Madina Airport to Madina Hotel",
+    initialCharge: "200/SAR",
+  },
+  {
+    title: "GMC 2016 to 2020 model",
+    location: "Madina Airport to Madina Hotel",
+    initialCharge: "300/SAR",
+  },
+  {
+    title: "GMC 2022 model",
+    location: "Madina Airport to Madina Hotel",
+    initialCharge: "400/SAR",
+  },
+  {
+    title: "HIACE",
+    location: "Madina Airport to Madina Hotel",
+    initialCharge: "300/SAR",
+  },
+  {
+    title: "COASTER",
+    location: "Madina Airport to Madina Hotel",
+    initialCharge: "500/SAR",
+  },
+  {
+    title: "BUS",
+    location: "Madina Airport to Madina Hotel",
+    initialCharge: "700/SAR",
+  },
+  {
+    title: "CAMRY SONATA",
+    location: "Madina Hotel to Madina Airport",
+    initialCharge: "120/SAR",
+  },
+  {
+    title: "H1 HYUNDAI",
+    location: "Madina Hotel to Madina Airport",
+    initialCharge: "150/SAR",
+  },
+  {
+    title: "GMC 2016 to 2020 model",
+    location: "Madina Hotel to Madina Airport",
+    initialCharge: "250/SAR",
+  },
+  {
+    title: "GMC 2022 model",
+    location: "Madina Hotel to Madina Airport",
+    initialCharge: "300/SAR",
+  },
+  {
+    title: "HIACE",
+    location: "Madina Hotel to Madina Airport",
+    initialCharge: "300/SAR",
+  },
+  {
+    title: "COASTER",
+    location: "Madina Hotel to Madina Airport",
+    initialCharge: "500/SAR",
+  },
+  {
+    title: "BUS",
+    location: "Madina Hotel to Madina Airport",
+    initialCharge: "700/SAR",
+  },
+  {
+    title: "CAMRY SONATA",
+    location: "Makkah Ziyarat",
+    initialCharge: "200/SAR",
+  },
+  {
+    title: "H1 HYUNDAI",
+    location: "Makkah Ziyarat",
+    initialCharge: "300/SAR",
+  },
+  {
+    title: "GMC 2016 to 2020 model",
+    location: "Makkah Ziyarat",
+    initialCharge: "350/SAR",
+  },
+  {
+    title: "GMC 2022 model",
+    location: "Makkah Ziyarat",
+    initialCharge: "500/SAR",
+  },
+  {
+    title: "HIACE",
+    location: "Makkah Ziyarat",
+    initialCharge: "400/SAR",
+  },
+  {
+    title: "COASTER",
+    location: "Makkah Ziyarat",
+    initialCharge: "600/SAR",
+  },
+  {
+    title: "BUS",
+    location: "Makkah Ziyarat",
+    initialCharge: "700/SAR",
+  },
+  {
+    title: "CAMRY SONATA",
+    location: "Madina Ziyarat",
+    initialCharge: "200/SAR",
+  },
+  {
+    title: "H1 HYUNDAI",
+    location: "Madina Ziyarat",
+    initialCharge: "250/SAR",
+  },
+  {
+    title: "GMC 2016 to 2020 model",
+    location: "Madina Ziyarat",
+    initialCharge: "350/SAR",
+  },
+  {
+    title: "GMC 2022 model",
+    location: "Madina Ziyarat",
+    initialCharge: "500/SAR",
+  },
+  {
+    title: "HIACE",
+    location: "Madina Ziyarat",
+    initialCharge: "400/SAR",
+  },
+  {
+    title: "COASTER",
+    location: "Madina Ziyarat",
+    initialCharge: "600/SAR",
+  },
+  {
+    title: "BUS",
+    location: "Madina Ziyarat",
+    initialCharge: "700/SAR",
+  },
+  {
+    title: "CAMRY SONATA",
+    location: "Jeddah Airport to Madina Hotel",
+    initialCharge: "500/SAR",
+  },
+  {
+    title: "H1 HYUNDAI",
+    location: "Jeddah Airport to Madina Hotel",
+    initialCharge: "600/SAR",
+  },
+  {
+    title: "GMC 2016 to 2020 model",
+    location: "Jeddah Airport to Madina Hotel",
+    initialCharge: "800/SAR",
+  },
+  {
+    title: "GMC 2022 model",
+    location: "Jeddah Airport to Madina Hotel",
+    initialCharge: "1000/SAR",
+  },
+  {
+    title: "HIACE",
+    location: "Jeddah Airport to Madina Hotel",
+    initialCharge: "650/SAR",
+  },
+  {
+    title: "COASTER",
+    location: "Jeddah Airport to Madina Hotel",
+    initialCharge: "1100/SAR",
+  },
+  {
+    title: "BUS",
+    location: "Jeddah Airport to Madina Hotel",
+    initialCharge: "1400/SAR",
+  },
+];
+
 const page = () => {
   return (
     <div>
@@ -7,115 +325,46 @@ const page = () => {
       <section className="our-taxi padding">
         <div className="container">
           <div className="row">
-            <div className="col-lg-4 col-md-6 sm-padding">
-              <div className="pricing-item">
-                <div className="pricing-head-wrap">
-                  <div className="pricing-car">
-                    <img src="/assets/img/pricing-car.png" alt="car" />
-                    <div className="price">$2.50/km</div>
+            {pricingData.map((item, index) => (
+              <div key={index} className="col-lg-4 col-md-6 sm-padding mb-4">
+                <div className="pricing-item">
+                  <div className="pricing-head-wrap">
+                    <div className="pricing-car">
+                      <img src="/assets/img/pricing-car.png" alt="car" />
+                      <div className="price">{item.initialCharge}</div>
+                    </div>
                   </div>
-                </div>
-                <div className="pricing-head">
-                  <h3>
-                    <a href="taxi-details.html">BMW X5 2008</a>
-                  </h3>
-                  <span className="location">Chicago</span>
-                </div>
-                <ul className="pricing-list">
-                  <li>
-                    Initial Charge: <span>$2.50</span>
-                  </li>
-                  <li>
-                    Per Mile/KM: <span>$4.20</span>
-                  </li>
-                  <li>
-                    Per Stopped Trafic: <span>$1.50</span>
-                  </li>
-                  <li>
-                    Passengers: <span>4 Person</span>
-                  </li>
-                  <li>
-                    <a href="book-taxi.html" className="default-btn">
-                      Book Taxi Now
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6 sm-padding">
-              <div className="pricing-item">
-                <div className="pricing-head-wrap">
-                  <div className="pricing-car">
-                    <img src="/assets/img/pricing-car.png" alt="car" />
-                    <div className="price">$3.50/km</div>
+                  <div className="pricing-head">
+                    <h3>
+                      <a href="taxi-details.html">{item.title}</a>
+                    </h3>
+                    <span className="location">{item.location}</span>
                   </div>
+                  <ul className="pricing-list">
+                    <li>
+                      Initial Charge: <span>{item.initialCharge}</span>
+                    </li>
+                    <li>
+                      Per Mile/KM: <span>$4.20</span>
+                    </li>
+                    <li>
+                      Per Stopped Traffic: <span>$1.50</span>
+                    </li>
+                    <li>
+                      Passengers: <span>4 Person</span>
+                    </li>
+                    <li>
+                      <a href="book-taxi.html" className="default-btn">
+                        Book Taxi Now
+                      </a>
+                    </li>
+                  </ul>
                 </div>
-                <div className="pricing-head">
-                  <h3>
-                    <a href="taxi-details.html">Mercedes‑Benz</a>
-                  </h3>
-                  <span className="location">Florida</span>
-                </div>
-                <ul className="pricing-list">
-                  <li>
-                    Initial Charge: <span>$2.50</span>
-                  </li>
-                  <li>
-                    Per Mile/KM: <span>$4.20</span>
-                  </li>
-                  <li>
-                    Per Stopped Trafic: <span>$1.50</span>
-                  </li>
-                  <li>
-                    Passengers: <span>4 Person</span>
-                  </li>
-                  <li>
-                    <a href="book-taxi.html" className="default-btn">
-                      Book Taxi Now
-                    </a>
-                  </li>
-                </ul>
               </div>
-            </div>
-            <div className="col-lg-4 col-md-6 sm-padding">
-              <div className="pricing-item">
-                <div className="pricing-head-wrap">
-                  <div className="pricing-car">
-                    <img src="/assets/img/pricing-car.png" alt="car" />
-                    <div className="price">$4.50/km</div>
-                  </div>
-                </div>
-                <div className="pricing-head">
-                  <h3>
-                    <a href="taxi-details.html">Hyundai 2022</a>
-                  </h3>
-                  <span className="location">New York</span>
-                </div>
-                <ul className="pricing-list">
-                  <li>
-                    Initial Charge: <span>$2.50</span>
-                  </li>
-                  <li>
-                    Per Mile/KM: <span>$4.20</span>
-                  </li>
-                  <li>
-                    Per Stopped Trafic: <span>$1.50</span>
-                  </li>
-                  <li>
-                    Passengers: <span>4 Person</span>
-                  </li>
-                  <li>
-                    <a href="book-taxi.html" className="default-btn">
-                      Book Taxi Now
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </section>
-      {/* <!--/.our-taxi--> */}
     </div>
   );
 };
