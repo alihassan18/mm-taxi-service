2a860e43b25996cc60081690dda70d5058e848cd
ajmalbhatti122 <ajmalbhatti1222@gmail.com>
2024-09-03 15:29:35 +0500

    seo tags added

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,28 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import HomePage from "./home/components/page";
+
+export const metadata = {
+  title: 'Reliable Umra Taxi Service for Pilgrims | MM Taxi Service',
+  description: 'Book our trusted Umra taxi service for convenient and safe transportation in Saudi Arabia. Get airport transfers, hotel transfers, and city tours with our experienced drivers.',
+  keywords: ['Umra taxi service', 'Saudi Arabia', 'airport transfers', 'hotel transfers', 'city tours'],
+  author: 'MM Taxi Service',
+  robots: 'index, follow',
+  viewport: 'width=device-width, initial-scale=1.0',
+  openGraph: {
+    title: 'Reliable Umra Taxi Service for Pilgrims | MM Taxi Service',
+    description: 'Book our trusted Umra taxi service for convenient and safe transportation in Saudi Arabia. Get airport transfers, hotel transfers, and city tours with our experienced drivers.',
+    url: 'https://www.mmtaxiservice.com',
+    siteName: 'MM Taxi Service',
+    type: 'website',
+    image: 'https://www.mmtaxiservice.com/assets/img/logo-dark.png',
+  },
+  twitter: {
+    card: 'summary',
+    title: 'Reliable Umra Taxi Service for Pilgrims | MM Taxi Service',
+    description: 'Book our trusted Umra taxi service for convenient and safe transportation in Saudi Arabia. Get airport transfers, hotel transfers, and city tours with our experienced drivers.',
+    image: 'https://www.mmtaxiservice.com/assets/img/logo-dark.png',
+  },
+};
+
 
 export default function Home() {
   return (
