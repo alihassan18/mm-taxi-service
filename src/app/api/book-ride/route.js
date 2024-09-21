348635bc813310ab9c4b6cd4c3332c75d9038262
ajmalbhatti122 <ajmalbhatti1222@gmail.com>
2024-09-21 23:54:05 +0500

    latest changes

--- src/app/api/book-ride/route.js
+++ src/app/api/book-ride/route.js
@@ -5,8 +5,8 @@ const transporter = nodemailer.createTransport({
   port: 587, // Replace with the correct port
   secure: false, // Set to true if using TLS
   auth: {
-    user: 'muhammadfaizan7986@gmail.com', // Replace with your email address
-    pass: 'rcffmlfsbiirgljc', // Replace with your email password
+    user: process.env.NEXT_PUBLIC_EMAIL, // Replace with your email address
+    pass: process.env.NEXT_PUBLIC_PASSWORD, // Replace with your email password
   },
 });
 
@@ -15,7 +15,7 @@ export async function POST(request) {
 
   const mailOptions = {
     from: email, // Replace with your email address
-    to: 'muhammadfaizan7986@gmail.com',
+    to: process.env.NEXT_PUBLIC_EMAIL,
     subject: 'Taxi Booking Request',
     html: `
       <h1>Taxi Booking Request</h1>
